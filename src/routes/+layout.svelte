d2bb026ca24b64dfe882b02e2a45aa586e91b1fc
veda velagapudi <21501a12c5@pvpsit.ac.in>
2024-09-22 17:02:38 +0530

    added tailwind css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Footer from '$lib/Footer.svelte';
+import Navbar from '$lib/Navbar.svelte'
+import "../app.css"
+</script>
+<Navbar/>
+<slot/>
+<Footer/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class = "underline font-bold text-blue-600">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
